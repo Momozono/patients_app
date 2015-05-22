93db1e6ea15db05cb27f4be5c487991e8d5283af
Momozono <tanatana1089@gmail.com>
2015-05-22 13:04:24 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PatientsApp::Application.config.secret_key_base = 'f15f648da9f5de1d125964e16b006b0d937ee7ff9da3a58f0e6e7064ae8568004636d0402fd832d5d698beea9c8790f331adfaac3c03d853eaede4ddf32c2cdb'
